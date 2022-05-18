e12735b3c96ae68c47d59028c584deed809ef8c8
Tim Whitbeck <tim.whitbeck@stord.com>
2022-05-18 16:22:06 -0400

    update page title

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "WEPCA",
   viewport: "width=device-width,initial-scale=1",
 });
 
