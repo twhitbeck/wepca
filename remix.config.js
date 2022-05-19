c560ee8a400859d202004fedeca1d522d3b643ae
Tim Whitbeck <tim.whitbeck@stord.com>
2022-05-19 07:12:24 -0400

    import some aspects of the indie stack (#1)
    * pull in pieces of the indie stack
    
    * add WebStorm project files

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
   serverBuildTarget: "cloudflare-pages",
   server: "./server.js",
   devServerBroadcastDelay: 1000,
